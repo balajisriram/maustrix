5e729048ed7e2bf2d72023b23e53de602cc92f4a
balaji <balajisriram@gmail.com>
2016-09-08 17:09:44 -0700

    made if-else into a switch case

--- classes/core/protocols/stimManagers/stimManager.m
+++ classes/core/protocols/stimManagers/stimManager.m
@@ -70,67 +70,59 @@ classdef stimManager
             playLoopSounds={};
             playSoundSounds={};
             
-            % nAFC/goNoGo setup:
-            if strcmp(trialManagerClass, 'nAFC') || ...
-                    strcmp(trialManagerClass,'biasedNAFC') || ...
-                    strcmp(trialManagerClass,'goNoGo') || ...
-                    strcmp(trialManagerClass,'oddManOut') || ...
-                    strcmp(trialManagerClass,'cuedGoNoGo') || ...
-                    strcmp(trialManagerClass,'changeDetectorTM')
-                % play trial start sound
-                if phase==1 && stepsInPhase <=0
-                    playSoundSounds{end+1} = {'trialStartSound', 50};
-                elseif strcmp(phaseType,'pre-request') && (any(ports(targetOptions)) || any(ports(distractorOptions)) || ...
-                        (any(ports) && isempty(requestOptions)))
-                    % play white noise (when responsePort triggered during phase 1)
-                    playLoopSounds{end+1} = 'trySomethingElseSound';
-                elseif ismember(phaseType,{'discrim','post-discrim','pre-response'}) && any(ports(requestOptions))
-                    % play stim sound (when stim is requested during phase 2)
-                    playLoopSounds{end+1} = 'keepGoingSound';
-                elseif strcmp(phaseType,'reinforced') && stepsInPhase <= 0 && trialDetails.correct
-                    % play correct sound
-                    playSoundSounds{end+1} = {'correctSound', msRewardSound};
-                elseif strcmp(phaseType,'reinforced') && stepsInPhase <= 0 && ~trialDetails.correct
-                    % play wrong sound
-                    playSoundSounds{end+1} = {'wrongSound', msPenaltySound};
-                elseif strcmp(phaseType,'earlyPenalty') %&& stepsInPhase <= 0 what does stepsInPhase do? I don't think we need this for this phase
-                    % play wrong sound
-                    playSoundSounds{end+1} = {'wrongSound', msPenaltySound};
-                end
-                
-                % freeDrinks setup
-                % this will have to be fixed for passiveViewing (either as a flag on freeDrinks or as a new trialManager)
-            elseif strcmp(trialManagerClass, 'freeDrinks') || strcmp(trialManagerClass, 'freeDrinksCenterOnly') || strcmp(trialManagerClass, 'freeDrinksSidesOnly') || strcmp(trialManagerClass, 'freeDrinksAlternate')
-                if phase==1 && stepsInPhase <=0
-                    playSoundSounds{end+1} = {'trialStartSound', 50};
-                elseif ismember(phaseType,{'discrim','pre-response'}) && ~isempty(targetOptions) && any(ports(setdiff(1:length(ports), targetOptions))) % normal freeDrinks
-                    % play white noise (when any port that is not a target is triggered)
-                    playLoopSounds{end+1} = 'trySomethingElseSound';
-                elseif ismember(phaseType,{'discrim','pre-response'}) && ~isempty(requestOptions) && any(ports(requestOptions)) % passiveViewing freeDrinks
-                    % check that the requestMode and requestRewardDone also pass
-                    % same logic as in the request reward handling, but for sound
-                    % play keepGoing sound?
-                    if playRequestSoundLoop
+            switch trialManagerClass()
+                case {'nAFC','biasedNAFC','goNoGo','oddManOut','cuedGoNoGo','changeDetectorTM'}
+                    % play trial start sound
+                    if phase==1 && stepsInPhase <=0
+                        playSoundSounds{end+1} = {'trialStartSound', 50};
+                    elseif strcmp(phaseType,'pre-request') && (any(ports(targetOptions)) || any(ports(distractorOptions)) || ...
+                            (any(ports) && isempty(requestOptions)))
+                        % play white noise (when responsePort triggered during phase 1)
+                        playLoopSounds{end+1} = 'trySomethingElseSound';
+                    elseif ismember(phaseType,{'discrim','pre-response'}) && any(ports(requestOptions))
+                        % play stim sound (when stim is requested during phase 2)
                         playLoopSounds{end+1} = 'keepGoingSound';
-                    end
-                end
-                % play correct/error sound
-                if strcmp(phaseType,'reinforced') && stepsInPhase <= 0
-                    if ~isempty(msRewardSound) && msRewardSound>0
+                    elseif strcmp(phaseType,'reinforced') && stepsInPhase <= 0 && trialDetails.correct
+                        % play correct sound
                         playSoundSounds{end+1} = {'correctSound', msRewardSound};
-                    elseif ~isempty(msPenaltySound) && msPenaltySound>0
+                    elseif strcmp(phaseType,'reinforced') && stepsInPhase <= 0 && ~trialDetails.correct
+                        % play wrong sound
+                        playSoundSounds{end+1} = {'wrongSound', msPenaltySound};
+                    elseif strcmp(phaseType,'earlyPenalty') %&& stepsInPhase <= 0 what does stepsInPhase do? I don't think we need this for this phase
+                        % play wrong sound
                         playSoundSounds{end+1} = {'wrongSound', msPenaltySound};
                     end
-                end
-            elseif ismember(trialManagerClass, {'autopilot','reinforcedAutopilot'})
-                % do nothing because we don't play any sounds in this case
-                if phase==1 && stepsInPhase <=0
-                    playSoundSounds{end+1} = {'trialStartSound', 50};
-                end
-            else
-                trialManagerClass
-                error('default getSoundsToPlay should only be for non-phased cases');
+                case {'freeDrinks','freeDrinksCenterOnly','freeDrinksSidesOnly','freeDrinksAlternate'}
+                    if phase==1 && stepsInPhase <=0
+                        playSoundSounds{end+1} = {'trialStartSound', 50};
+                    elseif ismember(phaseType,{'discrim','pre-response'}) && ~isempty(targetOptions) && any(ports(setdiff(1:length(ports), targetOptions))) % normal freeDrinks
+                        % play white noise (when any port that is not a target is triggered)
+                        playLoopSounds{end+1} = 'trySomethingElseSound';
+                    elseif ismember(phaseType,{'discrim','pre-response'}) && ~isempty(requestOptions) && any(ports(requestOptions)) % passiveViewing freeDrinks
+                        % check that the requestMode and requestRewardDone also pass
+                        % same logic as in the request reward handling, but for sound
+                        % play keepGoing sound?
+                        if playRequestSoundLoop
+                            playLoopSounds{end+1} = 'keepGoingSound';
+                        end
+                    end
+                    % play correct/error sound
+                    if strcmp(phaseType,'reinforced') && stepsInPhase <= 0
+                        if ~isempty(msRewardSound) && msRewardSound>0
+                            playSoundSounds{end+1} = {'correctSound', msRewardSound};
+                        elseif ~isempty(msPenaltySound) && msPenaltySound>0
+                            playSoundSounds{end+1} = {'wrongSound', msPenaltySound};
+                        end
+                    end
+                case {'autopilot','reinforcedAutopilot'}
+                    % do nothing because we don't play any sounds in this case
+                    if phase==1 && stepsInPhase <=0
+                        playSoundSounds{end+1} = {'trialStartSound', 50};
+                    end
+                otherwise
+                    error('default getSoundsToPlay should only be for non-phased cases');
             end
+            
             
             soundsToPlay = {playLoopSounds, playSoundSounds};
             
@@ -159,14 +151,14 @@ classdef stimManager
         function [t] = disp(stim)
             t = class(stim);
         end
-
+        
         function expertPostTrialCleanUp(stimManager)
             % this function is used in expert mode to perform user-defined cleanup tasks between phases
             % default behavior is to call Screen('Close') to clear all textures and close offscreen windows (but leave onscreen window!)
             
             Screen('Close')
         end
- 
+        
         % ==================================================================
         % HELPER FUNCTIONS
         
@@ -880,7 +872,7 @@ classdef stimManager
                     disp('stimManager:verifyLEDParamsOK::the cumulative fraction should sum to 1');
                     ok = ok && false;
                 end
-            end    
+            end
         end
         
         function out = getStandardLEDParams()

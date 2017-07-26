d1102a4b71832f0fdc95afdd306a688d7234a78c
balaji <balajisriram@gmail.com>
2017-07-26 16:51:54 -0700

    assignports is set to empty and added createStimSpecsFromParams for autopilot

--- classes/core/protocols/trialManagers/autopilot/autopilot.m
+++ classes/core/protocols/trialManagers/autopilot/autopilot.m
@@ -3,8 +3,8 @@ classdef autopilot<trialManager
 
     
     methods
-        function t=autopilot(soundManager, rewardManager, eyeController, frameDropCorner, dropFrames, ...
-                displayMethod, requestPort, saveDetailedFrameDrops, delayManager, responseWindowMs, showText)
+        function t=autopilot(sndMgr, reinfMgr, delayManager, frameDropCorner, dropFrames, requestPort, saveDetailedFrameDrops,...
+                responseWindowMs, customDescription, showText)
             % AUTOPILOT  class constructor.
             % t=autopilot(percentCorrectionTrials,soundManager,...
             %      rewardManager,[eyeController],[frameDropCorner],[dropFrames],[displayMethod],[requestPorts],[saveDetailedFramedrops],
@@ -16,7 +16,9 @@ classdef autopilot<trialManager
             % requestPorts
             assert(ismember(requestPort,{'none'}),'autopilot:autopilot:incompatibleValue','requestPort has to be ''none''');
             d=sprintf('autopilot');
-            t=t@trialManager(soundManager,rewardManager,eyeController,d,frameDropCorner,dropFrames,displayMethod,requestPort,saveDetailedFrameDrops,delayManager,responseWindowMs,showText);
+            t=t@trialManager(sndMgr,reinfMgr,delayManager, frameDropCorner,dropFrames,requestPort,saveDetailedFrameDrops,...
+                responseWindowMs,customDescription,showText);
+            
         end
         
         function out = getPercentCorrectionTrials(tm)
@@ -26,14 +28,6 @@ classdef autopilot<trialManager
         function out=getResponsePorts(trialManager,totalPorts)
 
             out=setdiff(1:totalPorts,getRequestPorts(trialManager,totalPorts));
-        end
-        
-        function out=stationOKForTrialManager(t,s)
-            if isa(s,'station')
-                out = getNumPorts(s)>=3;
-            else
-                error('need a station object')
-            end
         end
         
         function [tm, trialDetails, result, spec, rewardSizeULorMS, requestRewardSizeULorMS, ...
@@ -64,10 +58,138 @@ classdef autopilot<trialManager
     methods(Static)
         
         function [targetPorts, distractorPorts, details]=assignPorts(details,~,responsePorts)
-            temp = [1,3];
-            x = randperm(length(temp));
-            targetPorts=temp(x(1));
-            distractorPorts=setdiff(responsePorts,temp);
+            targetPorts=[];
+            distractorPorts=[];
+            details.targetPorts = [];
+            details.distractorPorts = [];
+        end
+        
+        function out=stationOKForTrialManager(~)
+            out = true;
+            % all stations should in theory be able to implement autopilot
+        end
+    end
+    
+    methods (Access = ?trialManager)
+        function [stimSpecs, startingStimSpecInd] = createStimSpecsFromParams(tm,stimList,targetPorts,distractorPorts,requestPorts,hz,indexPulses)
+            %	INPUTS:
+            %		trialManager - the trialManager object (contains the delayManager and responseWindow params)
+            %		stimList - cell array ::
+            %             { 'stimName1', stimParam1;
+            %               'stimName2', stimParams2;...}
+            %		targetPorts - the target ports NEEDS TO BE []
+            %		distractorPorts - the distractor ports NEEDS TO BE []
+            %		requestPorts - the request ports NEEDS TO BE[]
+            %		hz - the refresh rate of the current trial
+            %		indexPulses - something to do w/ indexPulses, apparently only during discrim phases
+            %	OUTPUTS:
+            %		stimSpecs, startingStimSpecInd
+            
+            % there are two ways to have no pre-request/pre-response phase:
+            %	1) have calcstim return empty preRequestStim/preResponseStim structs to pass to this function!
+            %	2) the trialManager's delayManager/responseWindow params are set so that the responseWindow starts at 0
+            %		- NOTE that this cannot affect the preOnset phase (if you dont want a preOnset, you have to pass an empty out of calcstim)
+            
+            stimNames = stimList(:,1);
+            stimParams = stimList(:,2);
+            
+            % nAFC con only have some stims.
+            %  - preDiscrimStim(can be empty, can be multiple)
+            %  - discrimStim(nonempty, 1 nos.)
+            %  - postDiscrimStim(can be empty, can be multiple)
+            
+            addedPreDicrimPhases = 0;
+            which = strcmp('preDiscrimStim',stimNames);
+            if ~isempty(stimParams{which})
+                addedPreDicrimPhases = addedPreDicrimPhases+length(stimParams{which});
+            end
+            
+            which = strcmp('discrimStim',stimNames);
+            validateattributes(stimParams{which},{'struct'},{'nonempty'});
+            
+            addedPostDiscrimPhases=0;
+            which = strcmp('postDiscrimStim',stimNames);
+            if ~isempty(stimParams{which})
+                addedPostDiscrimPhases=addedPostDiscrimPhases+length(stimParams{which});
+            end
+            
+            
+                       
+            % figure out the indices
+            preDiscrimIndex = 1;last = 1;
+            discrimIndex = last+1;last = last+1;
+            if addedPostDiscrimPhases
+                postDiscrimIndices = last+1:last+1+addedPostDiscrimPhases;
+                last = last+addedPostDiscrimPhases;
+            end
+            reinforcementIndex = last+1; last = last+1;
+            itlIndex = last+1;
+            
+            
+            % now generate our stimSpecs
+            startingStimSpecInd=1;
+            i=1;
+            
+            doNothing = [];
+            
+            % preDiscrim
+            if addedPreDicrimPhases
+                which = strcmp('preDiscrimStim',stimNames);
+                preDiscrimStim = stimParams{which};
+                for k = 1:length(preDiscrimStim) % loop through the pre discrim stims
+                    criterion={doNothing,i+1};                    
+                    if length(preDiscrimStim)>1
+                        preDiscrimName = sprintf('pre-discrim%d',k);
+                    else
+                        preDiscrimName = 'pre-discrim';
+                    end
+                    stimSpecs{i} = stimSpec(preDiscrimStim(k).stimulus,criterion,preDiscrimStim(k).stimType,preDiscrimStim(k).startFrame,...
+                        preDiscrimStim(k).framesUntilTimeout,preDiscrimStim(k).autoTrigger,preDiscrimStim(k).scaleFactor,false,hz,'post-discrim',preDiscrimName,...
+                        preDiscrimStim(k).punishResponses,false,[],preDiscrimStim(k).ledON,preDiscrimStim(k).soundPlayed);
+                    i=i+1;
+                end
+            end
+            
+            % discrim
+            criterion={doNothing,i+1};
+            which = strcmp('discrimStim',stimNames);
+            discrimStim = stimParams{which};
+            framesUntilTimeoutDiscrim = discrimStim.framesUntilTimeout;
+            stimSpecs{i} = stimSpec(discrimStim.stimulus,criterion,discrimStim.stimType,discrimStim.startFrame,...
+                discrimStim.framesUntilTimeout,discrimStim.autoTrigger,discrimStim.scaleFactor,false,hz,'discrim','discrim',...
+                false,true,indexPulses,discrimStim.ledON,discrimStim.soundPlayed); % do not punish responses here
+            i=i+1;
+            
+            % optional postDiscrim Phase
+            if addedPostDiscrimPhases
+                assert(~isinf(framesUntilTimeoutDiscrim),'autopilot:incompatbleParamValue','stimuli have to time out');
+                which = strcmp('postDiscrimStim',stimNames);
+                postDiscrimStim = stimParams{which};
+                for k = 1:length(postDiscrimStim) % loop through the post discrim stims
+                    criterion={doNothing,i+1}; % any response in any part takes you to the reinf
+                    if length(postDiscrimStim)>1
+                        postDiscrimName = sprintf('post-discrim%d',k);
+                    else
+                        postDiscrimName = 'post-discrim';
+                    end
+                    stimSpecs{i} = stimSpec(postDiscrimStim(k).stimulus,criterion,postDiscrimStim(k).stimType,postDiscrimStim(k).startFrame,...
+                        postDiscrimStim(k).framesUntilTimeout,postDiscrimStim(k).autoTrigger,postDiscrimStim(k).scaleFactor,false,hz,'post-discrim',postDiscrimName,...
+                        postDiscrimStim(k).punishResponses,false,[],postDiscrimStim(k).ledON,postDiscrimStim(k).soundPlayed);
+                    i=i+1;
+                end
+            end
+
+            % required reinforcement phase
+            criterion={[],i+1};
+            stimSpecs{i} = stimSpec([],criterion,'cache',0,[],[],0,false,hz,'reinforced','reinforcement',false,false,[],false,[]); % do not punish responses here, and LED is hardcoded to false (bad idea in general)
+            i=i+1;
+            
+            % required final ITL phase
+            which = strcmp('interTrialStim',stimNames);
+            interTrialStim = stimParams{which};
+            criterion={[],i+1};
+            stimSpecs{i} = stimSpec(interTrialStim.interTrialLuminance,criterion,'cache',0,interTrialStim.duration,[],0,true,hz,'itl','intertrial luminance',false,false,[],false,[]); % do not punish responses here. itl has LED hardcoded to false
+            i=i+1;
         end
     end
     
